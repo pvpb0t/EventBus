1b62c54a42bc9060bcb88dfd06e9a69cbe70c555
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2023-04-15 16:37:49 +0200

    added support for subevents

--- src/dev/pvpb0t/eventbus/EventManager.java
+++ src/dev/pvpb0t/eventbus/EventManager.java
@@ -18,85 +18,112 @@ import java.util.Map;
  */
 public class EventManager {
 
-    // ConcurrentHashMap used to store subscribers for thread-safe access
+    /**
+     * The subscribers map holds a list of listeners for each event type.
+     * The ConcurrentHashMap class is used to ensure thread-safety.
+     */
     private final ConcurrentHashMap<Class<? extends AbstractEvent>, ArrayList<DefaultListener>> subscribers = new ConcurrentHashMap<>();
 
     /**
      * Subscribes an object to events that it has specified methods for.
-     * Gets all declared methods of the object, checks for the presence of the EventHook annotation,
-     * and adds a DefaultListener to the corresponding list of the event class in the subscribers map.
+     * This method scans the object's class and its superclasses for methods that are annotated with @EventHook.
+     * For each such method, it creates a DefaultListener object with the method and the event class that it handles.
+     * The listener is added to the subscribers map for the corresponding event class.
+     * If the event class has any inner classes that extend it, the method is also added as a listener for those classes.
+     * 
      * @param object the object to be subscribed to events.
      */
     public void subscribe(final Object object) {
         Class<?> clazz = object.getClass();
-        for (Method method : clazz.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(EventHook.class)) {
-                final EventHook eventHook = method.getAnnotation(EventHook.class);
-                final int prio = eventHook.priority();
-                Class<?>[] parameterTypes = method.getParameterTypes();
-                if (parameterTypes.length > 0 && AbstractEvent.class.isAssignableFrom(parameterTypes[0])) {
-                    final Class<? extends AbstractEvent> eventClazz = (Class<? extends AbstractEvent>) method.getParameterTypes()[0];
-                    DefaultListener listener = new DefaultListener(eventClazz, method);
-                    listener.setPrio(prio);
-                    listener.setSource(object);
-                    subscribers.computeIfAbsent(eventClazz, k -> new ArrayList<>()).add(listener);
-                    subscribers.get(eventClazz).sort(new Comparator<DefaultListener>() {
-                        @Override
-                        public int compare(DefaultListener o1, DefaultListener o2) {
-                            return Integer.compare(o2.getPrio(), o1.getPrio());
+        while (clazz != null) {
+            for (Method method : clazz.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(EventHook.class)) {
+                    final EventHook eventHook = method.getAnnotation(EventHook.class);
+                    final int prio = eventHook.priority();
+                    Class<?>[] parameterTypes = method.getParameterTypes();
+                    if (parameterTypes.length > 0 && AbstractEvent.class.isAssignableFrom(parameterTypes[0])) {
+                        final Class<? extends AbstractEvent> eventClazz = (Class<? extends AbstractEvent>) parameterTypes[0];
+                        DefaultListener listener = new DefaultListener(eventClazz, method);
+                        listener.setPrio(prio);
+                        listener.setSource(object);
+                        subscribers.computeIfAbsent(eventClazz, k -> new ArrayList<>()).add(listener);
+                        if (eventClazz.getDeclaredClasses().length > 0) {
+                            for (Class<?> subClazz : eventClazz.getDeclaredClasses()) {
+                                if (eventClazz.isAssignableFrom(subClazz)) {
+                                    DefaultListener subListener = new DefaultListener((Class<? extends AbstractEvent>) subClazz, method);
+                                    subListener.setPrio(prio);
+                                    subListener.setSource(object);
+                                    subscribers.computeIfAbsent((Class<? extends AbstractEvent>) subClazz, k -> new ArrayList<>()).add(subListener);
+                                }
+                            }
                         }
-                    });
+                    }
                 }
             }
+            clazz = clazz.getSuperclass();
         }
     }
 
     /**
      * Hooks an event to all its subscribed listeners.
-     * Invokes the handle method of each DefaultListener that is subscribed to the event class of the given event.
+     * 
+     * This method retrieves the event class and iterates over its class hierarchy.
+     * For each class, it checks if the subscribers map contains any listeners for that class.
+     * If so, it creates a copy of the list of listeners and iterates over it.
+     * For each listener, it uses Java's MethodHandles API to invoke the method that handles the event.
+     * If an exception is thrown during the invocation, it is caught and printed to the standard error stream.
+     *
      * @param event the event to be hooked to listeners.
      */
     public void hook(AbstractEvent event) {
-        Class<? extends AbstractEvent> eventClass = event.getClass();
-        if (subscribers.containsKey(eventClass)) {
-            ArrayList<DefaultListener> listenersCopy = new ArrayList<>(subscribers.get(eventClass));
-            for (DefaultListener listener : listenersCopy) {
-                try {
-                    MethodHandles.Lookup lookup = MethodHandles.lookup();
-                    MethodHandle handle = lookup.unreflect(listener.getTarget());
-                    handle.invoke(listener.getSource(), event);
-                } catch (Throwable e) {
-                    e.printStackTrace();
+        Class<?> clazz = event.getClass();
+        while (clazz != null) {
+            if (subscribers.containsKey(clazz)) {
+                ArrayList<DefaultListener> listenersCopy = new ArrayList<>(subscribers.get(clazz));
+                for (DefaultListener listener : listenersCopy) {
+                    try {
+                        MethodHandles.Lookup lookup = MethodHandles.lookup();
+                        MethodHandle handle = lookup.unreflect(listener.getTarget());
+                        handle.invoke(listener.getSource(), event);
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+            clazz = clazz.getSuperclass();
         }
     }
 
 
-
-   /**
-    * Unsubscribes an object from all events that it has subscribed to.
-    *
-    * @param object the object to be unsubscribed from events.
-    */
-   public void unsubscribe(final Object object) {
-       Class<?> clazz = object.getClass();
-       for (Method method : clazz.getDeclaredMethods()) {
-           if (method.isAnnotationPresent(EventHook.class)) {
-               Class<?>[] parameterTypes = method.getParameterTypes();
-               if (parameterTypes.length > 0 && AbstractEvent.class.isAssignableFrom(parameterTypes[0])) {
-                   final Class<? extends AbstractEvent> eventClazz = (Class<? extends AbstractEvent>) method.getParameterTypes()[0];
-                   ArrayList<DefaultListener> listeners = subscribers.get(eventClazz);
-                   if (listeners != null) {
-                       listeners.removeIf(listener -> listener.getSource() == object);
-                       if (listeners.isEmpty()) {
-                           subscribers.remove(eventClazz);
-                       }
-                   }
-               }
-           }
-       }
+    /**
+     * Unsubscribes the given object from all events it was previously subscribed to.
+     * 
+     * This method removes all listeners that have the given object as their source from the subscribers map.
+     * It also removes any event classes that no longer have any listeners.
+     * If an event class has any inner classes that extend it, it also removes any listeners for those classes.
+     * 
+     * @param object the object to unsubscribe
+     */
+    public void unsubscribe(final Object object) {
+        subscribers.values().forEach(listeners -> listeners.removeIf(listener -> listener.getSource() == object));
+        subscribers.entrySet().removeIf(entry -> entry.getValue().isEmpty());
+        subscribers.keySet().stream()
+                .filter(clazz -> clazz.getDeclaredClasses().length > 0)
+                .flatMap(clazz -> Arrays.stream(clazz.getDeclaredClasses()))
+                .filter(subClazz -> subClazz.isAssignableFrom(subClazz.getDeclaringClass()))
+                .forEach(subClazz -> {
+                    ArrayList<DefaultListener> subListeners = subscribers.get(subClazz);
+                    if (subListeners != null) {
+                        subListeners.removeIf(listener -> listener.getSource() == object);
+                        if (subListeners.isEmpty()) {
+                            subscribers.remove(subClazz);
+                        }
+                    }
+                });
     }
+
+
+
 
 }
 
